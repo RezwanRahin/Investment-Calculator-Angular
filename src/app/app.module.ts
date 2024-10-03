bcd1f340c8cf91fc7d4d0b6b12d696d30a3536a6
rezwan <rezwanshahriorrahin@gmail.com>
2024-10-03 15:28:21 +0600

    create a module as 'user-input' and include into the 'app' module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,11 @@ import { BrowserModule } from "@angular/platform-browser";
 import { AppComponent } from "./app.component";
 import { HeaderComponent } from "./header/header.component";
 import { InvestmentResultsComponent } from "./investment-results/investment-results.component";
+import { UserInputModule } from "./user-input/user-input.module";
 
 @NgModule({
   declarations: [AppComponent, HeaderComponent, InvestmentResultsComponent],
-  imports: [BrowserModule],
+  imports: [BrowserModule, UserInputModule],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
